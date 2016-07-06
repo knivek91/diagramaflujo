97e2afb745710c4c167b75b83ea127db969035cc
Kevin Rojas Vargas <krojas@oc.ld.co.cr>
2016-07-06 00:08:40 -0600

    Second Commit

--- Diagra de Flujo/Public/js/main.js
+++ Diagra de Flujo/Public/js/main.js
@@ -1,1 +1,28 @@
 ﻿
+
+(function () {
+
+    var goJS = go.GraphObject.make;
+
+    var diagram = goJS(go.Diagram, "myDiagramDiv", {
+        initialContentAlignment: go.Spot.Center  // Center diagram contents
+        , "undoManager.isEnabled": true        // enable Ctrl-Z to undo and Ctrl-Y to redo
+    });
+
+    var Model = goJS(go.Model);
+
+    Model.nodeDataArray = [
+        { key: "Group1", isGroup: true },
+        { key: "Alpha", group: "Group1" },
+        { key: "Beta", group: "Group1" },
+        { key: "Gamma" }
+    ];
+
+    Model.linkDataArray = [
+      { from: "Alpha", to: "Beta" },
+      { from: "Group1", to: "Gamma" }
+    ];
+
+    diagram.model = Model;
+
+})();
