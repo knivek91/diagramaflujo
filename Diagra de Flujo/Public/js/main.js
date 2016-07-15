295bf0ac07c3fe5158b6afaefad5441174e5bf6a
Kevin Rojas Vargas <krojas@oc.ld.co.cr>
2016-07-14 23:00:07 -0600

    Create TreeView

--- Diagra de Flujo/Public/js/main.js
+++ Diagra de Flujo/Public/js/main.js
@@ -1,28 +1,39 @@
-﻿
-
-(function () {
+﻿(function () {
 
     var goJS = go.GraphObject.make;
 
     var diagram = goJS(go.Diagram, "myDiagramDiv", {
         initialContentAlignment: go.Spot.Center  // Center diagram contents
         , "undoManager.isEnabled": true        // enable Ctrl-Z to undo and Ctrl-Y to redo
+        , layout: goJS(go.TreeLayout, // specify a Diagram.layout that arranges trees
+                { angle: 90, layerSpacing: 35 })
     });
 
-    var Model = goJS(go.Model);
+    diagram.nodeTemplate =
+      goJS(go.Node, "Horizontal",
+        { background: "#44CCFF" },
+        goJS(go.Picture,
+          { margin: 10, width: 50, height: 50, background: "red" },
+          new go.Binding("source")),
+        goJS(go.TextBlock, "Default Text",
+          { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
+          new go.Binding("text", "name"))
+  );
 
-    Model.nodeDataArray = [
-        { key: "Group1", isGroup: true },
-        { key: "Alpha", group: "Group1" },
-        { key: "Beta", group: "Group1" },
-        { key: "Gamma" }
+    diagram.linkTemplate = goJS(go.Link, { routing: go.Link.Orthogonal, corner: 5 }, goJS(go.Shape, { strokeWidth: 3, stroke: "#555" })); // the link shape
+
+    var model = goJS(go.TreeModel);
+    model.nodeDataArray =
+    [ // the "key" and "parent" property names are required,
+      // but you can add whatever data properties you need for your app
+      { key: "1", name: "Master" },
+      { key: "2", parent: "1", name: "Son 1" },
+      { key: "3", parent: "1", name: "Son 2" },
+      { key: "4", parent: "3", name: "Son 2 1" },
+      { key: "5", parent: "3", name: "Son 2 2" },
+      { key: "6", parent: "2", name: "Son 1" }
     ];
 
-    Model.linkDataArray = [
-      { from: "Alpha", to: "Beta" },
-      { from: "Group1", to: "Gamma" }
-    ];
-
-    diagram.model = Model;
+    diagram.model = model;
 
 })();
